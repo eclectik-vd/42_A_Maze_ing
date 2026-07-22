e53f31f0c88a1b0e6c6f4a2ba6b2a0014278b200
Valerie Damaville <vadamavi@c2r10p5.42lehavre.fr>
2026-07-22 10:37:18 +0200

    Fix: File omitted from the previous commit

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,16 +1,19 @@
+from pathlib import Path
 import pytest
 
 from src.maze_app.parsing.config_parser import parse_config
 
 
-def test_file_not_found(tmp_path):
+def test_file_not_found(tmp_path: Path) -> None:
+
     config_file = tmp_path / "config.txt"
 
     with pytest.raises(FileNotFoundError):
         parse_config(str(config_file))
 
 
-def test_line_without_equals_sign(tmp_path):
+def test_line_without_equals_sign(tmp_path: Path) -> None:
+
     config_file = tmp_path / "config.txt"
     config_file.write_text("a line without the equals sign\n")
 
@@ -18,7 +21,8 @@ def test_line_without_equals_sign(tmp_path):
         parse_config(str(config_file))
 
 
-def test_line_without_equals_sign_v2(tmp_path):
+def test_line_without_equals_sign_v2(tmp_path: Path) -> None:
+
     config_file = tmp_path / "config.txt"
     config_file.write_text("a line without the equals sign\n")
 
@@ -26,21 +30,24 @@ def test_line_without_equals_sign_v2(tmp_path):
         parse_config(str(config_file))
 
 
-def test_empty_file(tmp_path):
+def test_empty_file(tmp_path: Path) -> None:
+
     config_file = tmp_path / "config.txt"
     config_file.write_text("")
 
     assert parse_config(str(config_file)) == {}
 
 
-def test_file_comments(tmp_path):
+def test_file_comments(tmp_path: Path) -> None:
+
     config_file = tmp_path / "config.txt"
     config_file.write_text("# only comments")
 
     assert parse_config(str(config_file)) == {}
 
 
-def test_file_empty_lines(tmp_path):
+def test_file_empty_lines(tmp_path: Path) -> None:
+
     config_file = tmp_path / "config.txt"
     config_file.write_text("#\n\n")
 
